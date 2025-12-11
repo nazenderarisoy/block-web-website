76ddec3e48fdde8280d4fe4b7003b725c0fc7208
nazenderarisoy <nazenderzarisoy@hotmail.com>
2025-12-11 03:41:22 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,20 @@
 // JavaScript Document
 console.log("hi");
 
+// constanten
+const openMenuButton = document.querySelector("header nav button");
+const deNav = document.querySelector("header nav");
+const sluitMenuButton = document.querySelector("header nav button");
+
+// functies
+function openMenu() {
+    deNav.classList.add("is-open");
+} 
+  
+function sluitMenu() {
+    deNav.classList.remove("is-open");
+}
+  
+// eventlisteners
+openMenuButton.onclick = openMenu;
+sluitMenuButton.onclick = sluitMenu;
